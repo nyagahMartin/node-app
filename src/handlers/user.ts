436755f9a1602481b58dd3a9aa1b9f5da8bea6b1
menyagah <menyagah27@gmail.com>
2023-07-25 14:39:07 +0300

    Add function to handle user signin

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,6 @@
+import { create } from 'domain';
 import prisma from '../db';
-import { hashPassword } from '../modules/auth';
+import { comparePassword, createJWT, hashPassword } from '../modules/auth';
 
 export const createNewUser = async (req, res) => {
 	const user = await prisma.user.create({
@@ -8,4 +9,24 @@ export const createNewUser = async (req, res) => {
 			password: await hashPassword(req.body.password),
 		},
 	});
+
+	const token = createJWT(user);
+	res.json({ token });
+};
+
+export const signin = async (req, res) => {
+	const user = await prisma.user.findUnique({
+		where: {
+			username: req.body.username,
+		},
+	});
+
+	const isValid = await comparePassword(req.body.password, user.password);
+	if (!isValid) {
+		res.status(401);
+		res.json({ message: 'username or password did not match' });
+		return;
+	}
+	const token = createJWT(user);
+	res.json({ token });
 };
