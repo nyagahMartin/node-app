f4a1048c794196e6a65feb38df3f698fd2cd7b69
menyagah <menyagah27@gmail.com>
2023-07-26 05:17:49 +0300

    Get product handlers

---
+++ src/handlers/product.ts
@@ -0,0 +1,27 @@
+import prisma from '../db';
+
+//get all
+export const getProducts = async (req, res) => {
+	const user = await prisma.user.findUnique({
+		where: {
+			id: req.user.id,
+		},
+		include: {
+			products: true,
+		},
+	});
+	res.json({ data: user.products });
+};
+
+//get one
+export const getOneProduct = async (req, res) => {
+	const id = req.params.id;
+
+	const product = await prisma.product.findFirst({
+		where: {
+			id,
+			belongsToId: req.user.id,
+		},
+	});
+	res.json({ data: product });
+};
