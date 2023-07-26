a9aea5576a23c331e4ba2566c120a9f233a13693
nyagahMartin <mnyaga27@yahoo.com>
2023-07-26 13:13:45 +0300

    modify code from require to imports and default exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const app = require('./server');
+import app from './server';
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
-})
+	console.log('Server running on port 3000');
+});
