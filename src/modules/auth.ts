33480c28f8d02e01cd9655eb22a9beee41a5112a
nyagahMartin <mnyaga27@yahoo.com>
2023-07-26 13:13:45 +0300

    Add authentication

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -11,3 +11,32 @@ export const createJWT = (user) => {
 
 	return token;
 };
+
+export const protect = (req, res, next) => {
+	const bearer = req.headers.authorization;
+	if (!bearer) {
+		res.status(401);
+		res.json({ message: 'You are not authorized' });
+		return;
+	}
+
+	const [, token] = bearer.split(' ');
+	if (!token) {
+		res.status(401);
+		res.json({ message: 'not a valid token' });
+		return;
+	}
+
+	try {
+		const user = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = user;
+		next();
+	} catch (e) {
+		console.log(e);
+		res.status(401);
+		res.json({
+			message: 'not a valid token',
+		});
+		return;
+	}
+};
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './router';
 import morgan from 'morgan';
 import cors from 'cors';
+import { protect } from './modules/auth';
 
 const app = express();
 
@@ -22,6 +23,6 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.use('/api', router);
+app.use('/api', protect, router);
 
 export default app;
