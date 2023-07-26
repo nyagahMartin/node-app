87434ee6cbae66fbb2575a4d1d4c94bfbf9832b0
nyagahMartin <mnyaga27@yahoo.com>
2023-07-26 20:36:07 +0300

    Add an update, update  handler

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -27,3 +27,30 @@ export const createUpdate = async (req, res) => {
 	});
 	res.json({ data: update });
 };
+
+export const updateUpdate = async (req, res) => {
+	const products = await prisma.product.findMany({
+		where: {
+			belongsToId: req.user.id,
+		},
+		include: {
+			updates: true,
+		},
+	});
+	const updates = products.reduce((allUpdates, product) => {
+		return [...allUpdates, ...product.updates];
+	}, []);
+
+	const match = updates.find((update) => update.id === req.params.id);
+
+	if (!match) {
+		res.json({ message: 'no updates' });
+	}
+
+	const updatedUpdate = await prisma.update.update({
+		where: {
+			id: req.params.id,
+		},
+		data: req.body,
+	});
+};
