d7007ed6976f66e687e410edf33a229897ab7408
menyagah <menyagah27@gmail.com>
2023-07-26 05:02:51 +0300

    Validate routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body, oneOf, validationResult } from 'express-validator';
 import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
@@ -19,7 +19,12 @@ router.put(
 	handleInputErrors,
 	(req, res) => {}
 );
-router.post('/product', () => {});
+router.post(
+	'/product',
+	body('name').isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 router.delete('/product/:id', () => {});
 
 /**
@@ -27,8 +32,20 @@ router.delete('/product/:id', () => {});
  */
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
+router.put(
+	'/update/:id',
+	body('title').optional(),
+	body('body').optional(),
+	body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+	body('version').optional(),
+	() => {}
+);
+router.post(
+	'/update',
+	body('title').exists().isString(),
+	body('body').exists().isString(),
+	() => {}
+);
 router.delete('/update/:id', () => {});
 
 /**
@@ -36,8 +53,19 @@ router.delete('/update/:id', () => {});
  */
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
+router.put(
+	'/updatepoint/:id',
+	body('name').optional().isString(),
+	body('description').optional().isString(),
+	() => {}
+);
+router.post(
+	'/updatepoint',
+	body('name').isString(),
+	body('description').isString(),
+	body('updateId').exists().isString(),
+	() => {}
+);
 router.delete('/updatepoint/:id', () => {});
 
 export default router;
