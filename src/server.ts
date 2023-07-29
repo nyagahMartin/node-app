ec5ac1bfaa0d29014ea1a1cbd218277da1554263
nyagahMartin <mnyaga27@yahoo.com>
2023-07-29 12:33:58 +0300

    Add error handlers

--- src/server.ts
+++ src/server.ts
@@ -26,8 +26,19 @@ app.post('/user', createNewUser);
 app.post('/signin', signin);
 
 app.use((err, req, res, next) => {
-	console.log(err);
-	res.json({ message: 'oops there was an error' });
+	if(err.type === 'auth') {
+		res.status(401).json({ message: 'unauthorized'})
+	} else if (err.type === 'input') {
+		res.status(400).json({
+			message: 'invalid input'
+		})
+	} else {
+		res.status(500).json(
+			{
+				message: 'Oops server went down'
+			}
+		)
+	}
 });
 export default app;
 
